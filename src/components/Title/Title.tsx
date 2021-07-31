e1311fe1015b62d432ce95cd06d3650ac9ade775
Krzysztof Pajk <chris.araneo@gmail.com>
2021-07-31 16:48:58 +0200

    Title component

---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import imageString from "../images/brush-header.png";
+import "./Title.scss";
+
+interface Props {
+  children?: any;
+}
+
+const Title: React.FC<Props> = (props: Props) => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = imageString;
+    image.onload = () => setIsLoading(false);
+  }, []);
+
+  return (
+    <h1
+      className="title"
+      style={{
+        backgroundImage: isLoading ? `url(${imageString})` : "",
+      }}
+    >
+      {props.children}
+    </h1>
+  );
+};
+
+export default Title;

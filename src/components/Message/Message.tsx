857e14ef3137480bcb9a6cbf8a7697600e3a1621
Krzysztof Pajk <chris.araneo@gmail.com>
2021-08-10 21:20:42 +0200

    Message component refactor

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -13,23 +13,25 @@ interface Props {
   children?: any;
 }
 
-const Message: React.FC<Props> = (props: Props) => {
-  return props.visible ? (
-    <article className={`message is-${props.type}`}>
-      <div className="message-header">
-        <p>{props.header}</p>
-        <button
-          className="delete"
-          aria-label="delete"
-          onClick={(event) => {
-            event.preventDefault();
-            props.onClick(event);
-          }}
-        ></button>
-      </div>
-      <div className="message-body">{String(props.children)}</div>
-    </article>
-  ) : null;
-};
+const Message: React.FC<Props> = (props: Props) => (
+  <>
+    {props.visible && (
+      <article className={`message is-${props.type}`}>
+        <div className="message-header">
+          <p>{props.header}</p>
+          <button
+            className="delete"
+            aria-label="delete"
+            onClick={(event) => {
+              event.preventDefault();
+              props.onClick(event);
+            }}
+          ></button>
+        </div>
+        <div className="message-body">{String(props.children)}</div>
+      </article>
+    )}
+  </>
+);
 
 export default Message;
